3919438a373f504d3b2d07e510d18a464afa1390
Waleed Ashraf <waleedashraf@outlook.com>
2018-06-06 05:44:08 +0500

    added pjax load/ side click fixes

--- contentScript.js
+++ contentScript.js
@@ -1,56 +1,64 @@
 
 var regex = /(https:\/\/github.com\/).*?(issues).*?(\d)/
+$(document).ready(function () {
 
-if (regex.test(window.location.href)) {
-  window.onload = function () {
+  function init() {
+    if (regex.test(window.location.href)) {
+      console.log('here3');
+      var title = document.getElementsByClassName('js-issue-title')[0].innerHTML
+      var des = document.getElementsByTagName('table')[0].innerHTML;
 
-    var title = document.getElementsByClassName('js-issue-title')[0].innerHTML
-    var des = document.getElementsByTagName('table')[0].innerHTML;
+      document.body.innerHTML += '<div class="github-title-main-div" style="display: none"> <div class="github-title-title"> <div class="github-title-title-text">' + title + '</div> <svg id="github-title-chevron-down" class="octicon octicon-chevron-down" viewBox="0 0 10 16" version="1.1" aria-hidden="true" width="20"> <path fill-rule="evenodd" d="M5 11L0 6l1.5-1.5L5 8.25 8.5 4.5 10 6l-5 5z"></path> </svg> <svg id="github-title-chevron-up" style="display: none" class="octicon octicon-chevron-up" viewBox="0 0 10 16" version="1.1" width="20" aria-hidden="true"> <path fill-rule="evenodd" d="M10 10l-1.5 1.5L5 7.75 1.5 11.5 0 10l5-5 5 5z"></path> </svg> </div> <div class="github-title-des"> <div class="github-title-des-text markdown-body"> ' + des + ' </div> </div> </div>';
 
-    document.body.innerHTML += '<div class="github-title-main-div" style="display: none"> <div class="github-title-title"> <div class="github-title-title-text">' + title + '</div> <svg id="github-title-chevron-down" class="octicon octicon-chevron-down" viewBox="0 0 10 16" version="1.1" aria-hidden="true" width="20"> <path fill-rule="evenodd" d="M5 11L0 6l1.5-1.5L5 8.25 8.5 4.5 10 6l-5 5z"></path> </svg> <svg id="github-title-chevron-up" style="display: none" class="octicon octicon-chevron-up" viewBox="0 0 10 16" version="1.1" width="20" aria-hidden="true"> <path fill-rule="evenodd" d="M10 10l-1.5 1.5L5 7.75 1.5 11.5 0 10l5-5 5 5z"></path> </svg> </div> <div class="github-title-des"> <div class="github-title-des-text markdown-body"> ' + des + ' </div> </div> </div>';
-
-    document.getElementById('github-title-chevron-down').onclick = function () {
-      document.getElementById("github-title-chevron-down").style.display = "none";
-      document.getElementById("github-title-chevron-up").style.display = "inline-block";
-      document.getElementsByClassName("github-title-des")[0].style.display = "block";
-    }
-
-    function hasClass(element, className) {
-      var regex = new RegExp('\\b' + className + '\\b');
-      do {
-        if (regex.exec(element.className || (element.target ? element.target.className : ''))) {
-          return true;
-        }
-        
-        element = element.parentNode || (element.target ? element.target.parentNode : element.target);
-      } while (element);
-      return false;
-    }
-    
-    function hide(e) {
-      if (!hasClass(e, 'discussion-timeline')) {
-        document.getElementById("github-title-chevron-up").style.display = "none";
-        document.getElementById("github-title-chevron-down").style.display = "inline-block";
-        document.getElementsByClassName("github-title-des")[0].style.display = "none";
+      document.getElementById('github-title-chevron-down').onclick = function () {
+        document.getElementById("github-title-chevron-down").style.display = "none";
+        document.getElementById("github-title-chevron-up").style.display = "inline-block";
+        document.getElementsByClassName("github-title-des")[0].style.display = "block";
       }
-    }
 
-    document.getElementById('github-title-chevron-up').onclick = function (){ hide(); };
-    document.getElementsByClassName('application-main')[0].onclick = function (e){ hide(e); };
+      function hasClass(element, className) {
+        var regex = new RegExp('\\b' + className + '\\b');
+        do {
+          if (regex.exec(element.className || (element.target !== undefined ? element.target.className : ''))) {
+            return true;
+          }
 
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      document.getElementsByClassName('github-title-main-div')[0].style.display = "block";
-    }
+          element = element.parentNode || (element.target !== undefined ? element.target.parentNode : element.target);
+        } while (element);
+        return false;
+      }
 
-    window.onscroll = function () {
+      function hide(e) {
+        if ( e == undefined || !hasClass(e, 'discussion-timeline')) {
+          document.getElementById("github-title-chevron-up").style.display = "none";
+          document.getElementById("github-title-chevron-down").style.display = "inline-block";
+          document.getElementsByClassName("github-title-des")[0].style.display = "none";
+        }
+      }
+
+      document.getElementById('github-title-chevron-up').onclick = function () { hide(); };
+      document.getElementsByClassName('application-main')[0].onclick = function (e) { hide(e); };
+
       if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         document.getElementsByClassName('github-title-main-div')[0].style.display = "block";
-      } else {
-        document.getElementsByClassName('github-title-main-div')[0].style.display = "none";
+      }
+
+      window.onscroll = function () {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+          document.getElementsByClassName('github-title-main-div')[0].style.display = "block";
+        } else {
+          document.getElementsByClassName('github-title-main-div')[0].style.display = "none";
+        }
       }
     }
   }
-}
+
+  $(document).on('pjax:success', function() {
+    init();
+  });
+  init();
+});
+
 
 /*
 <div class="github-title-main-div" style="display: none">
